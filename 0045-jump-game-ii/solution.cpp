f16114eb1955de72001fa23029d7a0babb17f816
njwu <97860082+komekko1123@users.noreply.github.com>
2023-02-09 15:57:42 +0000

    Sync LeetCode submission Runtime - 93 ms (13.39%), Memory - 16.6 MB (100.00%)

--- 0045-jump-game-ii/solution.cpp
+++ 0045-jump-game-ii/solution.cpp
@@ -2,22 +2,18 @@ class Solution {
 public:
     int jump(vector<int>& nums) {
       int i = 0, ans = 0;
-      int n = nums.size();
-      int j = n - 1;
-      vector<int> answer;
+      int j = nums.size()- 1;
       while( j != 0 ){
         for( i = 0 ; i < j ; i++){
-          if( i + nums[i] >= j ) 
-            break;        
+          if( i + nums[i] >= j ) {
+            ans++;
+            j = i;
+            i = -1;
+          } // if       
         } // for
-        answer.push_back(i);
-        j = i;
-      } // 
+      } // while
       
-      if ( n-1 > 0 )
-        return answer.size();
-      else 
-        return 0;
+      return ans;
     } // for
 
 
