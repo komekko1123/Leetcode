714a8e49667d505b7783868530212355cac95ae2
njwu <97860082+komekko1123@users.noreply.github.com>
2023-06-19 12:41:48 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.1 MB (100.00%)

---
+++ 1833-find-the-highest-altitude/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+      int high = 0;
+      vector<int> att(gain.size()+1);
+      att[0] = 0;
+      for(int i = 0 ; i < gain.size() ; i++ ){
+        high += gain[i];
+        att[i+1] = high;
+      } // for
+
+      return *max_element(att.begin(),att.end());;
+    }
+};
